04b97d2610ddd9c75e64136fb6ac551a167a5e11
abrahamsekar <43968887+abrahamsekar@users.noreply.github.com>
2022-02-25 00:08:46 +0530

    Add files via upload

---
+++ NegativeToPositive.java
@@ -0,0 +1,13 @@
+package week1.day2;
+
+public class NegativeToPositive {
+public static void main(String[] args) {
+	int number = -40;
+	int value = 0;
+	if(number < 0)
+	{
+		 value = number * -1;
+	}
+	System.out.println("The Number "+ number +" is converted to "+ value);
+}
+}
