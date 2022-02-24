1f81c00b387b7555a7d2b5762719ba9618183282
abrahamsekar <43968887+abrahamsekar@users.noreply.github.com>
2022-02-24 23:58:50 +0530

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,26 @@
+package week1.day2;
+
+public class Calculator {
+
+	public void add() {
+
+int numberOne = 10, numberTwo = 20, numberThree = 30;
+System.out.println(numberOne + numberTwo + numberThree);
+
+	}
+	public void sub() {
+		int firstNum = 20, secondNum = 15;
+		System.out.println(firstNum - secondNum);
+
+	}
+	public void mul() {
+		double valueOne= 5, valueTwo = 10;
+		System.out.println(valueOne * valueTwo);
+
+	}
+	public void div() {
+		float first = 35, second = 17;
+		System.out.println(first/second);
+
+	}
+}
---
+++ MyCalculator.java
@@ -0,0 +1,12 @@
+package week1.day2;
+
+public class MyCalculator {
+	public static void main(String[] args) {
+		Calculator obj = new Calculator();
+		obj.add();
+		obj.sub();
+		obj.mul();
+		obj.div();
+	}
+
+}
